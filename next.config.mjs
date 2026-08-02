e36210f488c492577cf895dab3c5d84b585fcf8d
Vishva-57 <Vishva-57@users.noreply.github.com>
2026-08-02 13:01:18 +0530

    Update serverExternalPackages in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["playwright", "@axe-core/playwright"],
   experimental: {
     serverComponentsExternalPackages: ["playwright", "@axe-core/playwright"],
   },
